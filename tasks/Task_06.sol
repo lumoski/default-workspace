73e0eec5a670008ec237c3c7f601fbfbc7e9b17e
Vladislav Pridchin <lumoskiofficial@gmail.com>
2026-02-05 21:51:31 +0300

    Пункт 7

--- tasks/Task_06.sol
+++ tasks/Task_06.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.0;
 
 contract Task_06 {
    uint256 public constant MAX_UINT = type(uint256).max; // Максимальное значение uint256
+   uint8 public constant MAX_UINT8 = type(uint8).max;
 
 
    function getMaxUint256() external pure returns (uint256) {
        return MAX_UINT; // Возвращает максимальное значение
+   }
+
+
+   function getMaxUint8() external pure returns (uint8) {
+       return MAX_UINT8; // Возвращаем максимальное значение, заданное константой
    }
 }
