3e9dc1d01c918209fccde3d757708fb79da47a53
Vladislav Pridchin <lumoskiofficial@gmail.com>
2026-02-05 20:18:07 +0300

    Пункт 4

--- tasks/Task_03.sol
+++ tasks/Task_03.sol
@@ -11,4 +11,8 @@ contract Task_03 {
    function addPerson(string memory _name, uint8 _age) external {
        people.push(Person(_name, _age)); // Добавление новой структуры в массив
    }
+
+   function getUser(uint _index) external view returns (string memory name, uint8 age) {
+       return (people[_index].name, people[_index].age);
+   }
 }
