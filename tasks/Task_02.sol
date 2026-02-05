d359af37e0d3dd1c15e4f83a5f85247794cc1b0c
Vladislav Pridchin <lumoskiofficial@gmail.com>
2026-02-05 20:12:41 +0300

    Пункт 3

--- tasks/Task_02.sol
+++ tasks/Task_02.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
 contract Task_02 {
-   uint[] public dynamicArray; // Динамический массив
-   uint[5] public fixedArray;   // Фиксированный массив из 5 элементов
+    uint[] public dynamicArray; // Динамический массив
+    uint[5] public fixedArray; // Фиксированный массив из 5 элементов
 
+    function addToDynamicArray(uint _value) external {
+        dynamicArray.push(_value); // Добавление элемента в динамический массив
+    }
 
-   function addToDynamicArray(uint _value) external {
-       dynamicArray.push(_value); // Добавление элемента в динамический массив
-   }
+    function sumArray() external view returns (uint256 sum) {
+        for (uint256 i = 0; i < dynamicArray.length; i++) {
+            sum += dynamicArray[i];
+        }
+    }
 }
