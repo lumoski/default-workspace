b7071e84db49ed8643befe22d10db323f91a94f7
Vladislav Pridchin <lumoskiofficial@gmail.com>
2026-02-08 02:11:18 +0300

    Лаб2 Пункт 9

--- tasks-lab2/Task_09.sol
+++ tasks-lab2/Task_09.sol
@@ -8,15 +8,20 @@ contract Task_09 {
    mapping(uint256 => string) public users;
    uint256 public userCount;
 
+   // Объявление событий
+   event UserAdded(uint256 indexed userId, string message);
+   event UserRemoved(uint256 indexed userId, string message);
 
    function addUser (string memory name) external {
        userCount++;
        users[userCount] = name;
+       emit UserAdded(userCount, "User has been added");
    }
 
 
    function removeUser (uint256 userId) external {
-       require(bytes(users[userId]).length != 0, "User  does not exist.");
+       require(bytes(users[userId]).length != 0, "User does not exist.");
        delete users[userId];
+       emit UserRemoved(userId, "User has been removed");
    }
 }
