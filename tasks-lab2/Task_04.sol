d3615f9d6beb9c166ae7d3bfc03a6c6ce2773668
Vladislav Pridchin <lumoskiofficial@gmail.com>
2026-02-08 00:53:20 +0300

    Лаб2 Пункт 4

--- tasks-lab2/Task_04.sol
+++ tasks-lab2/Task_04.sol
@@ -3,5 +3,17 @@ pragma solidity ^0.8.0;
 
 
 contract Task_04 {
-   
+    function generateFibo(uint256 n) external pure returns (uint256) {
+        uint256 a = 0;
+        uint256 b = 1;
+        uint256 next;
+    
+        do {
+            next = a + b;
+            a = b;
+            b = next;
+        } while (next <= n);
+    
+       return next;
+   }
 }
