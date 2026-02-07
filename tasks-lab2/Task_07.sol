d3f41d4caf02b95e210896175b951d090f2668e7
Vladislav Pridchin <lumoskiofficial@gmail.com>
2026-02-08 01:37:10 +0300

    Лаб2 Пункт 7

--- tasks-lab2/Task_07.sol
+++ tasks-lab2/Task_07.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 contract Task_07 {
     mapping(uint => string) public athletes;
+    uint public athletesCount;
     address public owner;
 
 
@@ -11,13 +12,7 @@ contract Task_07 {
         require(msg.sender == owner, "Only owner can call this function");
         _;
     }
-
-
-    function addAthlete(uint index, string memory athlete) private {
-        athletes[index] = athlete;
-    }
-
-
+    
     constructor() {
         owner = msg.sender;
         addAthlete(0, "John Doe");
@@ -25,41 +20,60 @@ contract Task_07 {
         addAthlete(2, "Mike Johnson");
     }
 
-
-    function updateAthlete(uint index, string calldata newAthlete) public {
-        string storage currentAthlete = athletes[index];
-        currentAthlete = newAthlete;
-    }
-
-
-    function replaceAllAthletes(string[] newAthletes) public onlyOwner {
-        require(newAthletes.length == 3, "Must provide exactly 3 athletes");
-        for (uint i = 0; i < 3; i++) {
-            athletes[i] = newAthletes[i];
+    // Публичная функция для добавления спортсмена
+    function addAthlete(uint index, string memory athlete) public onlyOwner {
+        require(index >= 0 && index <= 2, "Index must be between 0 and 2");
+        athletes[index] = athlete;
+        if (index >= athletesCount) {
+            athletesCount = index + 1;
         }
     }
 
-
-    function getAthlete(uint index) external returns (string memory) {
+    // Получение спортсмена по индексу
+    function getAthlete(uint index) public view returns (string memory) {
+        require(index >= 0 && index < athletesCount, "Invalid index");
         return athletes[index];
     }
 
-
-    function athleteExists(uint index) private view returns (bool) {
+    // Проверка существования спортсмена
+    function athleteExists(uint index) public view returns (bool) {
+        require(index >= 0 && index < athletesCount, "Invalid index");
         return bytes(athletes[index]).length > 0;
     }
 
-
-    function removeAthlete(uint index) public {
+    // Удаление спортсмена по индексу
+    function removeAthlete(uint index) public onlyOwner {
+        require(index >= 0 && index < athletesCount, "Invalid index");
         delete athletes[index];
+        // Можно уменьшить athletesCount, если удаляется последний элемент
+        if (index == athletesCount - 1) {
+            athletesCount--;
+        }
     }
 
-
+    // Получение всех спортсменов
     function getAllAthletes() public view returns (string[] memory) {
-        string[3] memory allAthletes;
-        for (uint i = 0; i < 3; i++) {
+        string[] memory allAthletes = new string[](athletesCount);
+        for (uint i = 0; i < athletesCount; i++) {
             allAthletes[i] = athletes[i];
         }
         return allAthletes;
+    }
+
+    // Обновление спортсмена
+    function updateAthlete(uint index, string calldata newAthlete) public onlyOwner {
+        require(index >= 0 && index < athletesCount, "Invalid index");
+        athletes[index] = newAthlete;
+    }
+
+    // Замена всех спортсменов
+    function replaceAllAthletes(string[] memory newAthletes) public onlyOwner {
+        require(newAthletes.length == 3, "Must provide exactly 3 athletes");
+        
+        for (uint i = 0; i < 3; i++) {
+            athletes[i] = newAthletes[i];
+        }
+
+        athletesCount = 3;
     }
 }
