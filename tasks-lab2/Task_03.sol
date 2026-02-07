8473ef099b867d73d1383f0f1ea1cf0dc1edafe3
Vladislav Pridchin <lumoskiofficial@gmail.com>
2026-02-08 00:44:07 +0300

    Лаб2 Пункт 3

--- tasks-lab2/Task_03.sol
+++ tasks-lab2/Task_03.sol
@@ -3,5 +3,15 @@ pragma solidity ^0.8.0;
 
 
 contract Task_03 {
-   
+    function generateFactorial(uint256 n) external pure returns (uint256) {
+        uint256 result = 1;
+        uint256 i = 1;
+
+        while (i <= n) {
+            result *= i;
+            i++;
+        }
+
+        return result;
+    }
 }
